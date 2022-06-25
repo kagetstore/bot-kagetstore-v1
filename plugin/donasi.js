b4a00ecdc644962d881b50c924b24e9cde79fa71
kagetstore <87493595+kagetstore@users.noreply.github.com>
2022-06-26 05:54:00 +0700

    Update and rename plugins/donasi.js to plugin/donasi.js

--- plugin/donasi.js
+++ plugin/donasi.js
@@ -1,7 +1,7 @@
 let handler = async m => m.reply(`
 ╭─「 Donasi • Pulsa 」
-│ •  [0895-3303-79186]
-│ •  [0896-5436-0447]
+│ •  [0812-7930-1794]
+│ •  [0857-6350-0823]
 ╰────
 `.trim()) // Tambah sendiri kalo mau
 handler.help = ['donasi']
